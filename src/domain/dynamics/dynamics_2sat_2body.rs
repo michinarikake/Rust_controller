8d5110039385ae7dace3665134ee2779d06f5e4f
michinarikake <>
2025-02-23 23:21:46 +0900

    fix 2sat dnamics

--- src/domain/dynamics/dynamics_2sat_2body.rs
+++ src/domain/dynamics/dynamics_2sat_2body.rs
@@ -21,12 +21,12 @@ impl ContinuousDynamics<PositionVelocityPairStateEci, Force3dEci> for PairTwoBod
         let r_vec_chief = state.chief().position();
         let v_vec_chief = state.chief().velocity();
         let r_norm_chief = state.chief().position_norm();
-        let a_vec_chief = -self.mu / (r_norm_chief.powi(3)) * r_vec_chief + input.get_vector();
+        let a_vec_chief = -self.mu / (r_norm_chief.powf(3.0)) * r_vec_chief;// ここ力入れてもいいかも。仮想の基準にするかどうか？
 
         let r_vec_deputy = state.deputy().position();
         let v_vec_deputy = state.deputy().velocity();
         let r_norm_deputy = state.deputy().position_norm();
-        let a_vec_deputy = -self.mu / (r_norm_deputy.powi(3)) * r_vec_deputy + input.get_vector();
+        let a_vec_deputy = -self.mu / (r_norm_deputy.powf(3.0)) * r_vec_deputy + input.get_vector();
 
         PositionVelocityPairStateEci::form_from_array(ndarray::concatenate![ndarray::Axis(0), v_vec_chief, a_vec_chief, v_vec_deputy, a_vec_deputy])
     }
